7832f8c85f6924e78345fa0e32c27f7915638c0c
julioofriski <julio.ffriski0@gmail.com>
2026-04-27 00:17:20 -0300

    arquivo para alterações no banco

--- deletes-e-inserts.py
+++ deletes-e-inserts.py
@@ -2,7 +2,10 @@ import sqlite3
 
 conexao = sqlite3.connect('database.db')
 cursor = conexao.cursor()
-sql_delete_query = 'DELETE FROM evento WHERE id = 5;'
-cursor.execute(sql_delete_query)
+sql_delete_column_query = '''
+ALTER TABLE lista
+DROP COLUMN comprovante;
+'''
+cursor.execute(sql_delete_column_query)
 conexao.commit()
 conexao.close()
